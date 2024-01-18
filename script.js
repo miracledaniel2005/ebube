c5186d5b297dac8a4bcd94b0205af005db95f655
miracledaniel2005 <144464615+miracledaniel2005@users.noreply.github.com>
2024-01-18 12:13:39 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
+function reveal() {
+    var reveals = document.querySelectorAll(".reveal");
+
+    for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 100;
+
+    if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("active");
+    } //else {
+        //reveals[i].classList.remove("active");
+    //}
+    }
+}
+
+window.addEventListener("scroll", reveal);
+
 window.onscroll = () => {
     const header  = document.querySelector("header");
     header.classList.toggle("sticky", window.scrollY > 100);
